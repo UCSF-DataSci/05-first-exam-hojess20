a4aa5a5ce84eec055f0f7ab467ac3b8d26cdc381
hojess20 <60207354+hojess20@users.noreply.github.com>
2024-10-23 13:04:51 -0700

    Updated readme file

--- setup_project.sh
+++ setup_project.sh
@@ -9,12 +9,12 @@ touch data/random_sequence.fasta
 echo "# Bioinformatics Project" > README.md
 echo "This project contains 3 subdirectories:" >> README.md
 echo "1. data" >> README.md
-echo " - random_sequence.fasta" >> README.md
+echo "	- random_sequence.fasta" >> README.md
 echo "2. results" >> README.md
 echo "	- cutsite_summary.txt" >> README.md
 echo "3. scripts" >> README.md
 echo "	- generate_fasta.py" >> README.md
 echo "	- dna_operations.py" >> README.md
-echo "  - find_cutsites.py" >> README.md
+echo "	- find_cutsites.py" >> README.md
 
 echo "Project directory structure created successfully."
